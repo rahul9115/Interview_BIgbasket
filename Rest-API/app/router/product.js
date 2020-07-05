7e5642fb82922fb77d6e8e2e60f978172ec0f2d8
unknown <sudharahul2010@gmail.com>
2020-07-05 15:41:42 +0530

    Better presentation

--- Rest-API/app/router/product.js
+++ Rest-API/app/router/product.js
@@ -3,11 +3,29 @@ const mongoose = require("mongoose");
 const Product = require("./model/product")
 const router = express.Router();
 router.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: "Handling Get requests to products"
-    })
-    Product.find().exec().then(doc => {
-        console.log(doc)
+
+    Product.find().select("name price _id").exec().then(doc => {
+        if (doc.length > 0) {
+            res.status(200).json({
+                count: doc.length,
+                products: doc.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:5000/products/" + doc._id
+                        }
+                    }
+                })
+
+            })
+        } else {
+            res.status(404).json({
+                messgae: "No entries found"
+            })
+        }
 
     }
     ).catch(err => console.log(err))
@@ -34,27 +52,55 @@ router.post("/", (req, res, next) => {
 })
 router.get("/:productId", (req, res, next) => {
     const id = req.params.productId;
-    Product.findById(id).exec().then(doc => {
+    Product.findById(id).select("name price products").exec().then(doc => {
         console.log(doc)
-        if (doc.length >= 0) {
-            res.status(200).json(doc)
-        } else {
-            res.status(404).json({
-                message: "No entries found"
+        if (doc) {
+            res.status(200).json({
+                product: doc,
+                request: {
+                    type: "GET",
+                    description: "Get all products",
+                    url: "http://localhost:5000/products"
+                }
             })
+        } else {
+            res.status(200).json({ message: "No such product found" })
         }
 
 
     }
+
+
+
     ).catch(err => console.log(err))
 })
 router.patch("/:productId", (req, res, next) => {
-    res.status(200).json({
-        message: "Updated Product"
-    })
+    const id = req.params.productId
+    const updatedops = {}
+    for (const ops of req.body) {
+        updatedops[ops.propName] = ops.value
+    }
+    Product.update({ _id: id }, { $set: updatedops }).exec().then(result => {
+        res.status(200).json({
+            message: "product updated",
+            request: {
+                type: "GET",
+                url: "http://localhost:5000/products/" + id
+            }
+        })
+    }).catch(err => console.log(err))
 })
 router.delete("/:productId", (req, res, next) => {
     const id = req.params.productId
-    Product.remove({ _id: id }).exec().then(result => { res.status(200).json(result) }).catch(err => console.log(err))
+    Product.remove({ _id: id }).exec().then(result => {
+        res.status(200).json({
+            message: "Product deleted",
+            request: {
+                type: "POST",
+                url: "http://localhost:5000/products",
+                data: { name: "String", price: "Number" }
+            }
+        })
+    }).catch(err => console.log(err))
 })
 module.exports = router;
